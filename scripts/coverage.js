08ae4bc154cf903b530f6a487bcbdcdb840bcd3a
fool2fish <fool2fish@gmail.com>
2014-09-19 16:02:19 +0800

    Add isUrl() and isKeyword().

--- scripts/coverage.js
+++ scripts/coverage.js
@@ -2,26 +2,26 @@ var format = require('util').format;
 var data = ''
 
 process.stdin.on('data', function(chunk) {
-    data += chunk
+  data += chunk
 })
 
 process.stdin.on('end', function() {
-    data = data.replace(/^[^{]*({)/g, '$1')
-    data = JSON.parse(data)
+  data = data.replace(/^[^{]*({)/g, '$1')
+  data = JSON.parse(data)
 
-    console.log('Coverage:')
-    data.files.sort(function(a, b) {
-        return a.coverage - b.coverage
-    }).forEach(function(item) {
-        console.log(format('%s%% - %s', pad(item.coverage), item.filename))
-    })
-    console.log()
+  console.log('Coverage:')
+  data.files.sort(function(a, b) {
+      return a.coverage - b.coverage
+  }).forEach(function(item) {
+      console.log(format('%s%% - %s', pad(item.coverage), item.filename))
+  })
+  console.log()
 })
 
 
 function pad(num) {
-    num = num.toFixed(2)
-    var len = 9 - num.length
-    return new Array(len).join(' ') + num
+  num = num.toFixed(2)
+  var len = 9 - num.length
+  return new Array(len).join(' ') + num
 }
 
--- tests/index-spec.js
+++ tests/index-spec.js
@@ -5,7 +5,7 @@ var fs = require('fs')
 var expect = require('expect.js')
 var commander = require('commander')
 
-var common = require('../index')
+var utilx = require('../index')
 
 
 describe('utilx', function(){
@@ -14,16 +14,16 @@ describe('utilx', function(){
 
         it('pass a string and return a split array', function() {
             var str = 'mac/chrome/10.0.0.1,firefox,sarafi/3.0'
-            var rt = common.split(str)
+            var rt = utilx.split(str)
             expect(rt.length).to.be(3)
             expect(rt[0]).to.be('mac/chrome/10.0.0.1')
         })
 
         it('pass any of other type and return an empty array', function() {
-            expect(common.split().length).to.be(0)
-            expect(common.split(false).length).to.be(0)
-            expect(common.split([1,2,3]).length).to.be(3)
-            expect(common.split({key:'value'}).length).to.be(0)
+            expect(utilx.split().length).to.be(0)
+            expect(utilx.split(false).length).to.be(0)
+            expect(utilx.split([1,2,3]).length).to.be(3)
+            expect(utilx.split({key:'value'}).length).to.be(0)
         })
     })
 
@@ -33,7 +33,7 @@ describe('utilx', function(){
             .option('--nick [s]', 'a name')
             .option('--favorite [s]', 'a favorite')
             .parse(['node', 'scriptpath', '--nick=fool2fish', '--favorite=imax'])
-        var rt = common.cGetCfg(commander)
+        var rt = utilx.cGetCfg(commander)
         expect(Object.keys(rt).length).to.be(2)
         expect(rt.nick).to.be('fool2fish')
         expect(rt.favorite).to.be('imax')
@@ -43,7 +43,7 @@ describe('utilx', function(){
 
         it('pass a not exist file and return an empty plain object', function() {
             var p = path.join(__dirname, 'path', 'to', 'not-existed-file.json')
-            var rt = common.readJSON(p)
+            var rt = utilx.readJSON(p)
             expect(Object.keys(rt).length).to.be(0)
             expect(JSON.stringify(rt)).to.be('{}')
         })
@@ -52,7 +52,7 @@ describe('utilx', function(){
             var p = path.join(__dirname, 'not-proper-file.txt')
             fs.writeFileSync(p, 'some text')
 
-            var rt = common.readJSON(p)
+            var rt = utilx.readJSON(p)
             expect(Object.keys(rt).length).to.be(0)
             expect(JSON.stringify(rt)).to.be('{}')
 
@@ -63,7 +63,7 @@ describe('utilx', function(){
             var p = path.join(__dirname, 'proper-file.json')
             fs.writeFileSync(p, '{"nick": "fool2fish", "job": "web developer"}')
 
-            var rt = common.readJSON(p)
+            var rt = utilx.readJSON(p)
             expect(Object.keys(rt).length).to.be(2)
             expect(rt.nick).to.be('fool2fish')
             expect(rt.job).to.be('web developer')
@@ -77,7 +77,7 @@ describe('utilx', function(){
 
         it('create it first if the file not exist', function() {
             var p = path.join(__dirname, 'path', 'to', 'not-existed-config.json')
-            common.writeJSON(p, {nick: 'fool2fish', blog: 'fool2fish.cn'})
+            utilx.writeJSON(p, {nick: 'fool2fish', blog: 'fool2fish.cn'})
 
             var cfg = require(p)
             expect(Object.keys(cfg).length).to.be(2)
@@ -93,7 +93,7 @@ describe('utilx', function(){
             var p = path.join(__dirname, 'existed-config.json')
             fs.writeFileSync(p, '{"nick": "fool2fish", "job": "web developer"}')
 
-            common.writeJSON(p, {nick: 'fool2fish', twitter: 'fool2fish'})
+            utilx.writeJSON(p, {nick: 'fool2fish', twitter: 'fool2fish'})
 
             var cfg = require(p)
             expect(Object.keys(cfg).length).to.be(2)
@@ -107,26 +107,25 @@ describe('utilx', function(){
     })
 
     it('camelcase', function() {
-        expect(common.camelcase('totoro')).to.be('totoro')
-        expect(common.camelcase('totoro-server')).to.be('totoroServer')
-        expect(common.camelcase('totoro-Common')).to.be('totoroCommon')
+        expect(utilx.camelcase('totoro')).to.be('totoro')
+        expect(utilx.camelcase('totoro-server')).to.be('totoroServer')
     })
 
     it('unCamelcase', function() {
-        expect(common.unCamelcase('totoro')).to.be('totoro')
-        expect(common.unCamelcase('totoroServer')).to.be('totoro-server')
-        expect(common.unCamelcase('TotoroCommon')).to.be('-totoro-common')
+        expect(utilx.unCamelcase('totoro')).to.be('totoro')
+        expect(utilx.unCamelcase('totoroServer')).to.be('totoro-server')
+        expect(utilx.unCamelcase('TotoroServer')).to.be('-totoro-server')
     })
 
     it('isExistedFile', function() {
         var existedFile = __filename
         var notExistedFile = path.join('path', 'to', 'not-existed-file.json')
 
-        expect(common.isExistedFile(existedFile)).to.be(true)
-        expect(common.isExistedFile(existedFile + '?querystring#hash')).to.be(true)
-        expect(common.isExistedFile(existedFile + '?querystring')).to.be(true)
-        expect(common.isExistedFile(existedFile + '#hash')).to.be(true)
-        expect(common.isExistedFile(notExistedFile)).to.be(false)
+        expect(utilx.isExistedFile(existedFile)).to.be(true)
+        expect(utilx.isExistedFile(existedFile + '?querystring#hash')).to.be(true)
+        expect(utilx.isExistedFile(existedFile + '?querystring')).to.be(true)
+        expect(utilx.isExistedFile(existedFile + '#hash')).to.be(true)
+        expect(utilx.isExistedFile(notExistedFile)).to.be(false)
     })
 
     describe('mix', function() {
@@ -134,26 +133,26 @@ describe('utilx', function(){
         describe('overwrite', function() {
 
             it('not overwrite', function() {
-                var rt = common.mix({}, {a:'first'}, {a:'second'}, false)
+                var rt = utilx.mix({}, {a:'first'}, {a:'second'}, false)
                 expect(rt.a).to.be('first')
             })
 
             it('overwrite', function() {
-                var rt = common.mix({}, {a:'first'}, {a:'second'}, true)
+                var rt = utilx.mix({}, {a:'first'}, {a:'second'}, true)
                 expect(rt.a).to.be('second')
             })
         })
 
         it('any of arguments could be null', function() {
-            var rt1 = common.mix(null, {a: 'first'}, false)
+            var rt1 = utilx.mix(null, {a: 'first'}, false)
             expect(Object.keys(rt1).length).to.be(1)
             expect(rt1.a).to.be('first')
 
-            var rt2 = common.mix({a: 'zero'}, null, {a: 'second'}, true)
+            var rt2 = utilx.mix({a: 'zero'}, null, {a: 'second'}, true)
             expect(Object.keys(rt2).length).to.be(1)
             expect(rt2.a).to.be('second')
 
-            var rt3 = common.mix({a: 'zero'}, {a: 'first'}, {a: 'second', b: 'second'})
+            var rt3 = utilx.mix({a: 'zero'}, {a: 'first'}, {a: 'second', b: 'second'})
             expect(Object.keys(rt3).length).to.be(2)
             expect(rt3.a).to.be('zero')
             expect(rt3.b).to.be('second')
@@ -162,13 +161,13 @@ describe('utilx', function(){
     })
 
     it('getExternalIpAddress', function() {
-        var rt = common.getExternalIpAddress()
+        var rt = utilx.getExternalIpAddress()
         var interfaces = require('os').networkInterfaces()
         var addresses = []
         Object.keys(interfaces).forEach(function(name) {
             addresses = addresses.concat(interfaces[name])
         })
-        var addresses = addresses.filter(function(node) {
+        addresses = addresses.filter(function(node) {
             if (node.family === 'IPv4' && node.internal === false) {
                 return true
             }
@@ -179,5 +178,25 @@ describe('utilx', function(){
         } else {
             expect(rt).to.be(null)
         }
+    })
+
+    it('isUrl', function() {
+      expect(utilx.isUrl(undefined)).to.be(false)
+      expect(utilx.isUrl(false)).to.be(false)
+      expect(utilx.isUrl(null)).to.be(false)
+      expect(utilx.isUrl('')).to.be(false)
+      expect(utilx.isUrl('a.com')).to.be(false)
+      expect(utilx.isUrl('http://a.com')).to.be(true)
+      expect(utilx.isUrl('https://a.com')).to.be(true)
+    })
+
+    it('isKeyword', function() {
+      expect(utilx.isKeyword(undefined)).to.be(false)
+      expect(utilx.isKeyword(false)).to.be(false)
+      expect(utilx.isKeyword(null)).to.be(false)
+      expect(utilx.isKeyword('http://a.com')).to.be(false)
+      expect(utilx.isKeyword('')).to.be(false)
+      expect(utilx.isKeyword('a/b/c')).to.be(false)
+      expect(utilx.isKeyword('abc')).to.be(true)
     })
 })
